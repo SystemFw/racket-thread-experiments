640c27298e0f04271bb68f2fc87916fb1ca8d60a
Fabio Labella <fabio.labella2@gmail.com>
2023-01-31 13:41:27 +0000

    Add comment on Racket distribution libs

--- threads.rkt
+++ threads.rkt
@@ -1,9 +1,13 @@
 #lang racket
 
 
+
 (begin
   (print "start")
   (thread (lambda ()
             (sleep 2)
             (print "thead done")))
   (print "spawner done"))
+
+
+;; https://racket.discourse.group/t/what-would-it-take-to-write-an-independent-racket-interpreter/951
