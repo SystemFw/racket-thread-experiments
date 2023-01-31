be2782245eb898a323463b513f2af3ee5a7d0392
Fabio Labella <fabio.labella2@gmail.com>
2023-01-31 23:45:08 +0000

    Add initial experiments on semaphore

--- threads.rkt
+++ threads.rkt
@@ -1,24 +1,5 @@
 #lang racket
 
-
-
-(define foo
-  (thunk
-   (println "start")
-   (thread (thunk
-            (sleep 2)
-            (println "thead done")))
-   (println "spawner done")))
-
-(define test-breaks-default
-  (thunk
-   (print "main thread")
-   (println (break-enabled))
-   (thread
-    (thunk
-     (print "spawned thread")
-     (println (break-enabled))))))
-
 ;; https://racket.discourse.group/t/what-would-it-take-to-write-an-independent-racket-interpreter/951
 
 ;; Designing promise:
@@ -36,3 +17,58 @@
 ;; ^ this is equivalent to uncancelable { poll => }
 ;; breaks are enabled by default
 ;; for events, there is a `sync/enable-break` form
+
+
+(define basic-thread
+  (thunk
+   (println "start")
+   (thread (thunk
+            (sleep 2)
+            (println "thead done")))
+   (println "spawner done")))
+
+(define test-breaks-default
+  (thunk
+   (print "main thread")
+   (println (break-enabled))
+   (thread
+    (thunk
+     (print "spawned thread")
+     (println (break-enabled))))))
+
+
+;; deterministically prints done-3-2-1
+(define test-spawn-order
+  (thunk
+   (thread (thunk (println 1)))
+   (thread (thunk (println 2)))
+   (thread (thunk (println 3)))
+   (println "done")))
+
+;; deterministically prints 2-1-done-3
+(define test-spawn-order-2
+  (thunk
+   (thread (thunk (println 1)))
+   (thread (thunk (println 2)))
+   (sleep 1)
+   (thread (thunk (println 3)))
+   (println "done")))
+
+
+;; As predicted, the second spawned thread hangs
+;; (they spawn in reverse order, so "foo" hangs)
+(define test-semaphore-no-broadcast
+  (thunk
+   (define sem (make-semaphore))
+   (define event (semaphore-peek-evt sem))
+   (define (thread-logic name)
+     (thunk
+      (printf "Thread ~a started ~n" name)
+      (semaphore-wait sem)
+      (printf "Thread ~a finished ~n" name)))
+   (println "Main thread started")
+   (thread (thread-logic "foo"))
+   (thread (thread-logic "bar"))
+   (sleep 3)
+   (semaphore-post sem)
+   (println "Main thread finished")))
