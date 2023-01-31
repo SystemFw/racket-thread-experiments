f2a2305dc0df6f642ed3a6233f3f3e393b7fe99d
Fabio Labella <fabio.labella2@gmail.com>
2023-01-31 20:06:11 +0000

    Prototype for a Promise built on Deferre[Unit]

---
+++ promise.scala
@@ -0,0 +1,44 @@
+//> using lib "org.typelevel::cats-effect::3.4.5"
+
+
+import cats.effect._
+import cats.syntax.all._
+
+object Ex extends IOApp.Simple {
+  def run = IO.println("hello")
+}
+
+trait Promise[A] {
+  def read: IO[A]
+  def write(a: A): IO[Boolean]
+  def tryRead: IO[Option[A]]
+}
+object Promise {
+  def create[A]: IO[Promise[A]] =
+    (IO.ref(Option.empty[A]), IO.deferred[Unit]).mapN { case (state, waiters) =>
+      new Promise[A] {
+        def read: IO[A] =
+          state.get.flatMap {
+            case Some(a) => IO.pure(a)
+            case None =>
+              waiters.get >> read
+          }
+
+        def write(a: A): IO[Boolean] =
+          state.access.flatMap { case (state, set) =>
+            state match {
+              case Some(_) => IO.pure(false)
+              case None =>
+                set(a)
+                  .flatTap { noConflict => waiters.complete(()).whenA(noConflict) }
+                  .uncancelable
+                  .flatMap { noConflict =>
+                    if (noConflict) ()
+                    else write(a)
+                  }.as(true)
+            }
+          }
+        def tryRead: IO[Option[A]] = state.get
+      }
+    }
+}
